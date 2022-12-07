2a682e66f041fcde75ac16941743af276a40cf50
Abdiel Corro <acorrop@outlook.com>
2022-12-07 10:17:53 -0600

    exercise completed

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Abdiel";
+let lastName = "Corro";
+var age = 38;
 
 // Exercise 2
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
 
 // Exercise 3
+let city = "Green Bay";
+let pastime = "reading"
+let myStory = `Hello! My name is ${fullName}. I live in ${city}. I enjoy ${pastime} and coding!`;
+console.log(myStory);
